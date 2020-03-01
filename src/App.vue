cded56903fb30604b92a9973f98bc37862d580e1
ITWhiteRaccoon <eduardo.a@edu.pucrs.br>
2020-02-29 23:51:52 -0300

    muse-ui

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,39 @@
 <template lang="html">
-    <div>
-        <vs-table :data="posts">
-            <template slot="header">
-                <h3>Posts</h3>
-            </template>
-            <template slot="thead">
-                <vs-th></vs-th>
-            </template>
-        </vs-table>
+    <div id="app">
+        <mu-container>
+            <mu-paper :z-depth="1">
+                <mu-data-table :columns="columns" :sort.sync="sort" @sort-change="handleSortChange" :data="posts">
+                    <template slot="expand" slot-scope="prop">
+                        <div><div></div></div>
+                    </template>
+                </mu-data-table>
+            </mu-paper>
+        </mu-container>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'App',
-      data:()=>({posts})
+        data() {
+            return {
+                posts: [
+                    {
+                        "userId": 1,
+                        "id": 1,
+                        "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+                    },
+                    {
+                        "userId": 1,
+                        "id": 2,
+                        "title": "qui est esse",
+                        "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+                    }
+                ]
+            }
+        }
     };
 </script>
 
-<style lang="stylus">
-    .con-expand-users
-        .con-btns-user
-            display flex
-            padding 10px
-            padding-bottom 0px
-            align-items center
-            justify-content space-between
-
-            .con-userx
-                display flex
-                align-items center
-                justify-content flex-start
-
-        .list-icon
-            i
-                font-size .9rem !important
+<style>
 </style>
--- src/main.js
+++ src/main.js
@@ -1,22 +1,13 @@
 import Vue from 'vue';
-import Vuesax from 'vuesax';
-import 'vuesax/dist/vuesax.css';
-import 'material-icons/iconfont/material-icons.css';
 import App from './App.vue';
+import MuseUI from 'muse-ui';
+import 'muse-ui/dist/muse-ui.css'
+import 'material-icons/iconfont/material-icons.css';
+
+require('typeface-roboto');
 
 Vue.config.productionTip = false;
-Vue.use(Vuesax, {
-    rtl: false,
-    theme: {
-        colors: {
-            primary: '#8ccc0b',
-            success: '#17c964',
-            danger: '#f2135d',
-            warning: '#ff8200',
-            dark: '#242145'
-        }
-    }
-});
+Vue.use(MuseUI);
 
 new Vue({
     render: h => h(App)
